a871d09fed3a3e71d9540f88650182c6f321a212
tclack88 <tclack@aol.com>
2025-05-23 14:49:51 +0800

    added RGB arduino rainbow potentiometer code and PNG

---
+++ potentiometer/RGB_rainbow_potentiometer.ino
@@ -0,0 +1,104 @@
+// RGB rainbow ith potentiometer
+
+/* previous INCORRECT assumption: 
+red starts at a max. Then,
+red drops while green rises simultaneously (like a seesaw)
+Then green hits a max
+Then green steadily drops while blue rises (like a seesaw)
+Then blue is at a max
+Then blue sreadily drops while red simultaneously rises (like a seesaw)
+
+The strategy: divide 1024 into 3 parts (~340). Then in each third section,
+take care of the two colors while maintaining the third at zero.
+The (incorrect) code for that strategy:
+
+  if (detect < 340){ //red -> green
+    greenval = detect*255/340; // green increase
+    redval = (340-detect)*255/340; //red decrease
+    blueval = 0;
+  }
+  else if (detect >= 340 && detect <= 680){ //green -> blue
+    greenval = (680-detect)*255/340;// green decrease
+    blueval = (detect-340)*255/340;// blue increase;
+    redval = 0;
+  }
+  else if (detect > 680 ){ // blue -> red
+    blueval = (1023 - detect)*255/342; // blue decrease
+    redval= (detect - 680)*255/342; // red increase
+    greenval = 0;
+  }
+
+Based on seeing the values using a hex colorcode picker here:
+https://htmlcolorcodes.com/color-picker/
+I can see that what actually happens is
+Red starts at a max
+Green steadily rises to a max (while red is maintained constant)
+Then Green is maintained constant while red is steadily dropped
+Now green is at a max
+Blue steadily rises while green remains constant
+Blue hits a max (with green)
+Green is steadily dropped
+Blue is at a max
+Red steadily rises wile Blue is maintained constant
+Red and Blue are both at a max
+Blue steadily decreases while red is maintained constant
+Red is at a max
+*/
+int red = 9;
+int green = 5;
+int blue = 3;
+
+void setup() {
+  pinMode(red,OUTPUT);
+  pinMode(green,OUTPUT);
+  pinMode(blue,OUTPUT);
+  pinMode(A0,INPUT);
+}
+
+void loop() {
+  int detect = analogRead(A0);
+  int redval;
+  int greenval;
+  int blueval;
+  if (detect <= 146){ // off -> red (+red)
+    redval = (detect)*255/146; // +red
+    greenval = 0;
+    blueval = 0;
+  }
+  else if (detect > 146 && detect <= 292){ //red -> yellow (+green)
+    redval = 255;
+    greenval = (detect - 146)*255/146; // +green
+    blueval = 0;
+  }
+  else if (detect > 292 && detect <= 438 ){ // yellow -> green (-red)
+    redval= (438 - detect)*255/146; // -red
+    greenval = 255;
+    blueval = 0; // blue decrease
+  }
+  else if (detect > 438 && detect <= 584 ){ // green -> cyan (+blue)
+    redval = 0;
+    greenval = 255;
+    blueval = (detect-438)*255/146; // +blue
+  }
+  else if (detect > 584 && detect <= 730 ){ // cyan -> blue (-green)
+    redval = 0;
+    greenval = (730-detect)*255/146;
+    blueval = 255;
+  }
+  else if (detect > 730 && detect <= 876 ){ // blue -> magenta (+red)
+    redval = (584-detect)*255/146;
+    greenval = 0;
+    blueval = 255;
+  }
+  else{ // 877-1023magenta -> fade out (-red and blue)
+    redval = (1023-detect)*255/146; // (-red)
+    greenval = 0;
+    blueval = (1023-detect)*255/146; // (-blue)
+  }
+  
+  delay(10); // make it less "jumpy"
+  analogWrite(red,redval);
+  analogWrite(green,greenval);
+  analogWrite(blue,blueval);
+
+}
