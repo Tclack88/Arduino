b6ae1ea0670bc722c0c09724e6a646b39af87777
tclack88 <tclack@aol.com>
2025-10-19 23:10:41 -0700

    arduino client code updated to include more complex behavior with sending average values and send continuously over UDP until a cancel request is sent. Added png of the both components

--- Multiple/Rpi_ESP32_UDP_transmit_server.ino
+++ Multiple/Rpi_ESP32_UDP_transmit_server.ino
@@ -11,33 +11,50 @@
 const char* ssid = WIFI_SSID;
 const char* pw = WIFI_PW;
 int port = 12005;
+#define iLED 2 // internal LED
 
 int sensor = 34;
 int val = 0;
+int sending = 0; // toggle 1 or 0. Keep track if sending to UDP or not
 float voltage = 0.0;
 
 char packet_received[255]; // Buffer to hold any incoming packets
 WiFiUDP UDP;
 
 int I = 0;
-int data[5] = {0,0,0,0,0};
+float data[5] = {0.0,0.0,0.0,0.0,0.0};
 
 // float R1 = 1000; // TODO: measure for more precision
 
-float read_photoresistor(){
-  int avg = 0;
+float avg_photoresistor(){
+  float sum = 0.0;
   for (int i=0; i<5; i++)
-    avg += data[i];
-  return ((float) avg) / 5;
+    sum += data[i];
+  return sum / 5;
 }
 
-void check_request(){
-  // look if UDP request made over wifi.
-  // if so, call read_photoresistor()
+int check_request(){
+  int packetSize = UDP.parsePacket();
+  // Serial.printf("checking for packet... size: %d",packetSize);
+  return packetSize; // 0 if no packet (button press), else positive
+}
+
+void collect(){
+  val = analogRead(sensor);
+  voltage = ( (float)val /4095 )*3.3;
+  data[I] = voltage;
+  I = (I+1)%4;
+}
+
+void send(){
+  UDP.beginPacket(UDP.remoteIP(),UDP.remotePort());
+  UDP.print(avg_photoresistor());
+  UDP.endPacket();
 }
 
 void setup() {
-  // pinMode(sensor,INPUT);
+  pinMode(iLED,OUTPUT);
+  // pinMode(sensor,INPUT); // apparently this is not needed
   Serial.begin(9600);
   // connect to network. Display IP
   WiFi.begin(ssid,pw);
@@ -57,15 +74,44 @@ void loop() {
   if (packetSize){
     int len = UDP.read(packet_received,sizeof(packet_received)-1);
     packet_received[len] = 0; // Null terminate
-    val = analogRead(sensor);
-    voltage = ( (float)val )*3.3/4095;
     Serial.print("UDP request received from ");
     Serial.print(UDP.remoteIP());
     Serial.printf(". Packetsize: %d, Message: %s.\n",packetSize, packet_received);
+    // sending = (sending + 1)%2;
+    sending = 1;
+    for (int i=0; i<5; i++){ // collect initial 5 packets
+      collect();
+      delay(1000);
+    }
+    send(); // send first sample of 5
+    unsigned long now = millis();
+    unsigned long blink_timer = now; // timer for blink. Ever 0.5 seconds toggle
+    unsigned long send_timer = now; // timer to send. ever 2 seconds send
+    unsigned long collect_timer = now; // timer for every 1 second
+    while (sending){
+      unsigned long now = millis();
+      if (int r = check_request()){ // button pressed again, cancel
+        Serial.printf("\tRequest received: %d",r);
+        sending = 0; 
+        UDP.beginPacket(UDP.remoteIP(),UDP.remotePort());
+        UDP.print("-1");
+        Serial.println("cancelled, no more sending information");
+        UDP.endPacket();
+        UDP.begin(port); // re-Begin UDP listener
+      }
+      if ( (now - send_timer) >= 2000 ){
+        send();
+        send_timer = now; // reset
+        Serial.printf("(data sent)(val=%d)\n",val);
+      }
+      if ( (now - collect_timer) >= 1000){
+        collect();
+        collect_timer = now;
+      }
+      // TODO: blink_tiemr
+      delay(50); // short enough to catch button press, but still save energy
 
-    UDP.beginPacket(UDP.remoteIP(),UDP.remotePort());
-    UDP.print(voltage);
-    UDP.endPacket();
+    }
 
   // TODO: work on logic for average value,  for now, just return current reading
   //   data[I] = val;
