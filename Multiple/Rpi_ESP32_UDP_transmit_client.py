61dea8650034b1bcc9deabc2617f6b813d0b6df2
tclack88 <tclack@aol.com>
2025-10-22 23:38:12 -0700

    update Rpi code to include fuller functionality, starting, stopping and flahsing LED warning

--- Multiple/Rpi_ESP32_UDP_transmit_client.py
+++ Multiple/Rpi_ESP32_UDP_transmit_client.py
@@ -45,18 +45,18 @@ GPIO.cleanup()
 """
 def blink_white(blinking):
     while True:
-        print(blinking.value)
-        if blinking.value:
-            if GPIO.input(button) != 0:
+        #print(blinking.value)
+        if blinking.value == 1:
+            if GPIO.input(button):
                 blinking.value = 0
-                print("breaking out of blinking")
-        GPIO.output(white,True)
-        time.sleep(.5)
-        GPIO.output(white,False)
-        time.sleep(.5)
+                receiving = 0
+            GPIO.output(white,True)
+            time.sleep(.5)
+            GPIO.output(white,False)
+            time.sleep(.5)
 
-    else:
-        time.sleep(.1)
+        else:
+            time.sleep(.1)
 
 blink_process = mp.Process(target=blink_white, args=(blinking,))
 blink_process.start()
@@ -70,7 +70,7 @@ def light_LED(val):
         GPIO.output(green,True)
     elif floor(val) == 2:
         GPIO.output(yellow,True)
-    else:
+    elif floor(val) == 1:
         GPIO.output(red,True)
 
 ESP_ip = '192.168.1.108' # changes
@@ -80,36 +80,49 @@ ESP_port = 12005
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP
 server_socket.settimeout(10.0)
 
-while True:
-    if receiving == 0 and blinking.value == 0: # "off" not primed to receive
-        while GPIO.input(button) == 0:
-            GPIO.output(white,receiving)
-            time.sleep(.1)
-    time.sleep(.3)
-    receiving = 1
-    GPIO.output(white,receiving)
-   
+def send_message():
     message = time.ctime() # simple date stamp for time of request
     server_socket.sendto(message.encode('utf-8'), (ESP_ip, ESP_port) )
+
+
+while True:
+    while (blinking.value == 1):
+        time.sleep(.1)
+    if receiving == 0: # "off" not primed to receive
+        light_LED(5)
+        GPIO.output(white,receiving)
+        while GPIO.input(button) == 0:
+            time.sleep(.1)
+    #if blinking.value == 1:
+    #    blinking.value = 0
+    #    continue # restart
+    blinking.value = 0;
+    receiving = 1
+   
+    send_message()
+    time.sleep(.5) # buffer (avoid double press counted)
     while (receiving):
         receiving = (receiving + GPIO.input(button)) % 2 #check for button press to cancel after loop ends
         GPIO.output(white,receiving)
-        if not receiving:
-            receiving = 0
-            break
-        print(f"receiving: {receiving}")
+        time.sleep(0.3) # buffer (avoid double press counted)
+        if not receiving: #if button pressed, cancel
+            send_message()
+        #print(f"receiving: {receiving}")
         try:
             resp_message, addr = server_socket.recvfrom(1024)
             val = float(resp_message.decode('utf-8'))
             if val < 0:
                 print("END")
                 receiving = 0 
-                print(f"receiving: {receiving}")
+                light_LED(5) #5 is outside range of 0-3, so it will turn off
+                #print(f"receiving: {receiving}")
             else:
                 print(f"sucessful receipt of message from {addr[0]}. message: {resp_message} (type:{type(resp_message)})")
                 light_LED(val)
 
         except Exception as e:
+            send_message()
             blinking.value = 1
             receiving = 0
             print(f"FAILURE: {e}")
+
