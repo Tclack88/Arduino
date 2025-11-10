c1f2b7e67fec132af083e65e65d698c785e80a72
tclack88 <tclack@aol.com>
2025-11-09 21:52:40 -0800

    update comments. Remove unecessary print statements and unused variables

--- Multiple/Rpi_ESP32_ESP32_swarm_server.ino
+++ Multiple/Rpi_ESP32_ESP32_swarm_server.ino
@@ -10,48 +10,50 @@
 
 const char ID = 3;
 
+const int STORAGE_CAPACITY = 1; // set array size for collecting last N voltages
 const char* ssid = WIFI_SSID;
 const char* pw = WIFI_PW;
-int rpi_port = 12005;
+int rpi_port = 12005; // external communication to Rpi
 int swarm_port = 3000; // internal communicatin for ESPs
-#define iLED 2 // internal LED
+#define iLED 2 // internal LED pin for ESP32 is always pin 2
 bool IS_MASTER = false;
 
 bool iLED_status = false;
-int sensor = 34;
-// int val = 0;
+int sensor = 34; // pin to read photoresistor
+
 int sending = 0; // toggle 1 or 0. Keep track if sending to UDP or not
-// float voltage = 0.0;
+int highest_voltage = 0; // global. Used to determine blink rate
+
 IPAddress myIP;
-char myIP_string[16];
 char brother1IP[16];
 char brother2IP[16];
-char* brothers[] = {brother1IP,brother2IP};
+char* brothers[] = {brother1IP,brother2IP}; // hold up to 2 other ESP32 "brothers"
 int swarm_size = 1;
 
 char packet_received[255]; // Buffer to hold any incoming packets
 WiFiUDP PI_UDP;
 WiFiUDP ESP_UDP;
 
-int I = 0;
-float data[5] = {0.0,0.0,0.0,0.0,0.0};
+int I = 0; // global I (index pointer for data array)
+float data[STORAGE_CAPACITY] = {0.0};
 
 float avg_photoresistor(){
   float sum = 0.0;
-  for (int i=0; i<5; i++)
+  for (int i=0; i<STORAGE_CAPACITY; i++)
     sum += data[i];
-  return sum / 5;
+  return sum / STORAGE_CAPACITY;
 }
 
-void collect(){
+void collect(){ // update voltage reading
   int val = analogRead(sensor);
-  float voltage = ( (float)val /4095 )*3.3;
+  float voltage = 3.3-( (float)val /4095 )*3.3;
+  if (voltage > highest_voltage)
+    highest_voltage = voltage; // update global highest_voltage
   data[I] = voltage;
-  // Serial.printf("DEBUG: val: %d, voltage: %f (%f %f %f %f %f)\n",val, voltage, data[0], data[1], data[2], data[3], data[4]);
-  I = (I+1)%5;
+  I = (I+1)%STORAGE_CAPACITY;
 }
 
-void send(){
+void send(){ // send ID and Voltage to Raspberry Pi
   PI_UDP.beginPacket(PI_UDP.remoteIP(),PI_UDP.remotePort());
   char message_out[255];
   sprintf(message_out, "%d %f", ID, avg_photoresistor());
@@ -66,14 +68,12 @@ void toggle_iLED(){
 }
 
 char parse_packet(char packet_received[]){
-  // Changes the "brothers" list (who is available)
-  // printf("packet received: %s\n",packet_received);
-  char cmd;
-  int size;
-  int num_chars = 0;
+  // update the "brothers" list (who is on network)
+  char cmd; // 'C' (continue/commence) or 'E' (end)
+  int size; // number of total ESPs on network
+  int num_chars = 0; // message size, needed for parsing array for other ESPs
   char ip_addr[16]; // 255.255.255.255 15 char max + nullbyte                                                                                                                             
   sscanf(packet_received, "%c %d %n", &cmd, &size, &num_chars); // maybe change to fgets (255 buffer size) for "safety"
-  // printf("cmd: %c, size: %d\n", cmd, size);
   swarm_size = size; // update locally known count
   if (cmd == 'C'){
     int brother = 0;
@@ -93,7 +93,6 @@ char parse_packet(char packet_received[]){
 }
 
 bool determine_master(char* brothers[]){
-  // delay(10*ID); // delay 10,20 or 30 ms to give time for sending/receiving (necessary? Trying to ensure simultaneously sent/received messages are not lost)
   char local_packet_received[255]; // Buffer to hold any incoming packets
   float myVal = avg_photoresistor();
   float largest = myVal;
@@ -101,16 +100,15 @@ bool determine_master(char* brothers[]){
   for (int i=0; i<swarm_size-1; i++){
     IPAddress brotherIP;
     brotherIP.fromString(brothers[i]);
-    // Serial.printf("\t checking brother: "); Serial.println(brotherIP);
-    ESP_UDP.beginPacket(brotherIP,swarm_port); // TODO: double check ip address format
+    ESP_UDP.beginPacket(brotherIP,swarm_port);
     ESP_UDP.print(myVal);
     ESP_UDP.endPacket();
-    // ESP_UDP.begin(swarm_port); // re-Begin UDP listener
     int esp_packetSize = ESP_UDP.parsePacket();
     if (esp_packetSize){
+      Serial.print("receeiving...");
       int len = ESP_UDP.read(local_packet_received,sizeof(local_packet_received)-1);
       local_packet_received[len] = 0; // Null terminate
-      // Serial.printf("local packet message: %s\n", local_packet_received);
+      Serial.println(" ...read");
       float val = atof(local_packet_received); 
       largest = max(largest, val);
       Serial.print("\n\t brother:");Serial.print(brotherIP); Serial.print("  ");Serial.println(val);
@@ -118,20 +116,15 @@ bool determine_master(char* brothers[]){
     }
   }
   if (myVal == largest)
-    return true; // IS_MASTER set to true
-  return false; // IS_MASTER set to false
+    return true; // IS_MASTER = true
+  return false; // IS_MASTER = false
 }
 
 char check_request(int packetSize){
   // update contents of packet_received (last Rpi message)
   // Calls parse_packet to "update brother's"
-  // int packetSize = PI_UDP.parsePacket();
-  // return packetSize; // 0 if no packet (button press), else positive
   int len = PI_UDP.read(packet_received,sizeof(packet_received)-1);
   packet_received[len] = 0; // Null terminate
-  // Serial.print("UDP request received from ");
-  // Serial.print(PI_UDP.remoteIP());
-  // Serial.printf(". Packetsize: %d, Message: %s.\n",packetSize, packet_received);
   char cmd = parse_packet(packet_received); // establishes brothers, receives cmd ('C' or 'E')
   return cmd;
 }
@@ -151,53 +144,44 @@ void setup() {
   Serial.printf(" Wifi connected\n IP address: ");
   myIP = WiFi.localIP();
   Serial.println(myIP);
-  PI_UDP.begin(rpi_port); // Begin UDP listener
-  ESP_UDP.begin(swarm_port); // Begin UDP listener for internal (swarm) netwwork
+  PI_UDP.begin(rpi_port); // Begin external UDP listener (to Raspberry Pi) 
+  ESP_UDP.begin(swarm_port); // Begin internal UDP listener (to swarm netwwork)
 }
 
 void loop() {
+  collect(); // Collection still happens even when not "IS_MASTER"
   int packetSize = PI_UDP.parsePacket();
-  if (packetSize){  // add "or" condition check for cancellation
-    // int len = PI_UDP.read(packet_received,sizeof(packet_received)-1);
-    // packet_received[len] = 0; // Null terminate
-    // Serial.print("UDP request received from ");
-    // Serial.print(PI_UDP.remoteIP());
-    // Serial.printf(". Packetsize: %d, Message: %s.\n",packetSize, packet_received);
-    // parse_packet(packet_received); // establishes brothers, receives cmd ('C' or 'E')
+  if (packetSize){
     char cmd = check_request(packetSize); 
     IS_MASTER = determine_master(brothers);
     if (IS_MASTER){
       sending = 1;
       unsigned long now = millis();
-      unsigned long blink_timer = now; // timer for blink. Ever 0.5 seconds toggle
-      unsigned long send_timer = now; // timer to send. ever 2 seconds send
-      unsigned long collect_timer = now; // timer for every 1 second
+      unsigned long blink_timer = now; // timer for blink. Toggle based on voltage read
+      unsigned long send_timer = now; // timer to send. every 1s, send
+      unsigned long collect_timer = now; // timer for reading photoresistor. every 300ms
       while (sending){
-        // Sending stopped if IS_MASTER returns false or a button press is received while sneding as the master
-        packetSize = PI_UDP.parsePacket();
-        if (packetSize){
-          cmd = check_request(packetSize);
+        packetSize = PI_UDP.parsePacket(); // check for update from Rpi
+        if (packetSize){ // if non-empty (message received)
+          cmd = check_request(packetSize); // cmd = 'C' or 'E'
           IS_MASTER = determine_master(brothers);
-          sending = (IS_MASTER && cmd == 'C'); // end if end signal 'E' is received
+          sending = (IS_MASTER && cmd == 'C'); // both'C' received AND IS_MASTER => send
           if (cmd == 'E'){ // cancel 
             PI_UDP.beginPacket(PI_UDP.remoteIP(),PI_UDP.remotePort());
-            PI_UDP.print("-1 -1");
-            // Serial.println("cancelled, no more sending information");
+            PI_UDP.print("-1 -1"); // return -1 ID and -1 voltage (no LED or voltage/blink rate set)
             PI_UDP.endPacket();
-            // PI_UDP.begin(rpi_port); // re-Begin UDP listener
           }
         }
         now = millis();
-        if ( (now - send_timer) >= 2000 ){
+        if ( (now - send_timer) >= 1000 ){
           send();
           send_timer = now; // reset
-          // Serial.printf("(data sent)(val=%d)\n",val);
         }
-        if ( (now - collect_timer) >= 1000){
+        if ( (now - collect_timer) >= 300){
           collect();
           collect_timer = now;
         }
-        if ( (now - blink_timer) >= 500){
+        if ( (now - blink_timer) >= 1000*(highest_voltage - avg_photoresistor()) ){ // variable blink rate
           toggle_iLED();
           blink_timer = now;
         }
